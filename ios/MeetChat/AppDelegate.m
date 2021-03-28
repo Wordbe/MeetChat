d24d3bb982087b153426c97f85a51cee7fa09c0a
Wordbe <seonghojin3@gmail.com>
2021-03-28 23:35:58 +0900

    fix: RCTBridge deadlock problem related to react-native-vector-icons

--- ios/MeetChat/AppDelegate.m
+++ ios/MeetChat/AppDelegate.m
@@ -25,6 +25,10 @@ static void InitializeFlipper(UIApplication *application) {
 }
 #endif
 
+#if RCT_DEV
+#import <React/RCTDevLoadingView.h>
+#endif
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -37,10 +41,20 @@ static void InitializeFlipper(UIApplication *application) {
   InitializeFlipper(application);
 #endif
 
+  // problem: RCTBridge required dispatch_sync to load RCTDevLoadingView. This may lead to deadlocks
+  // cause: react-native-vector-icons
+  // solution below
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"MeetChat"
-                                            initialProperties:nil];
+  #if RCT_DEV
+    [bridge moduleForClass:[RCTDevLoadingView class]];
+  #endif
+    RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                    moduleName:@"MeetChat"
+                                              initialProperties:nil];
+  // RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  // RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+  //                                                  moduleName:@"MeetChat"
+  //                                           initialProperties:nil];
 
   if (@available(iOS 13.0, *)) {
       rootView.backgroundColor = [UIColor systemBackgroundColor];
